2639c778d2fa75a8687bcedeab386efdcad5b2bc
laura <laura@lauramackenzie.net>
2017-01-06 01:35:14 -0500

    updated fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,25 @@
 //Go through numbers 1-20 and call fizzBuzz function to determine output
+  document.writeln('<pre>');
 for (i=1; i<=20; i++) {
   console.log(fizzBuzz(i));
   document.writeln(fizzBuzz(i));
 }
+  document.writeln('</pre>');
 
   function fizzBuzz(number) {
   //If current number is divisible by 3, but it's not divisible by 5, return fizz
-  if (i % 3 === 0) {
+  if (i % 3 ===0) {
     if (i % 5 !== 0) {
-      return'fizz';
+      return'Fizz';
     }
     else {
       //if it is divisible by 5, return fizzbuzz
-      return'fizbuzz';
+      return'FizzBuzz';
     }
   }
   //If current number is not divisible by 5, but is divisible by 5, return buzz
   else if (i % 5 === 0) {
-    return 'buzz';
+    return 'Buzz';
   }
     //If current number is divisible by neither 3 nor 5, return number
   else {
